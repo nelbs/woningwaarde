1186cff13c7e8fea752f60110e7a592bbcc66d64
Nick Elbers <nick.elbers@rhdhv.com>
2022-03-07 10:31:42 +0100

    Update sensor.py

--- custom_components/woningwaarde/sensor.py
+++ custom_components/woningwaarde/sensor.py
@@ -64,7 +64,7 @@ class Woningwaarde(RestoreEntity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         # Return the state attributes.
         return self._attributes
 
